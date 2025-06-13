8b5844cbddaff2fdd4c5cd8a1c9dc9ce1dddbfac
Gilbert Carrasco <tonytrox@gmail.com>
2025-06-12 22:38:30 -0500

    Fix path: Add request and error logs in JSON format

--- backend/Middlewares/logger.js
+++ backend/Middlewares/logger.js
@@ -3,13 +3,13 @@ const expressWinston = require("express-winston");
 
 // crear logger de solicitudes
 const requestLogger = expressWinston.logger({
-  transports: [new winston.transports.File({ filename: "request.log" })],
+  transports: [new winston.transports.File({ filename: "../request.log" })],
   format: winston.format.json(),
 });
 
 // crear logger de errores
 const errorLogger = expressWinston.errorLogger({
-  transports: [new winston.transports.File({ filename: "error.log" })],
+  transports: [new winston.transports.File({ filename: "../error.log" })],
   format: winston.format.json(),
 });
 
